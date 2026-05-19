21aefc0d87793c045e89a2d47e1086ea96d069e2
laianapinho <laiana.cavalcante15@gmail.com>
2026-05-19 10:44:57 -0400

    fix: run playwright in headless mode

--- test_google.py
+++ test_google.py
@@ -2,7 +2,7 @@ from playwright.sync_api import sync_playwright
 
 def test_google():
     with sync_playwright() as p:
-        browser = p.chromium.launch(headless=False)
+        browser = p.chromium.launch()
         page = browser.new_page()
 
         page.goto("https://google.com")
